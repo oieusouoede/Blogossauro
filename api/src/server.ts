9b338d86d30b70eb0a9f2c266419a6c11f8fca58
Edgar Marinho <edemarinho@outlook.com>
2023-12-23 20:02:32 -0300

    Configuração do banco, users model e users controller

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,29 +1,41 @@
-import "./util/module-alias";
-import { Server } from "@overnightjs/core";
-import bodyParser from "body-parser";
-import { EntriesController } from "./controllers/entries";
-import { Application } from "express";
+import './util/module-alias';
+import { Server } from '@overnightjs/core';
+import bodyParser from 'body-parser';
+import { EntriesController } from './controllers/entries';
+import { Application } from 'express';
+import * as database from '@src/database';
+import { UsersController } from './controllers/users';
 
 export class SetupServer extends Server {
-	constructor(private port = 3000) {
-		super();
-	}
+  constructor(private port = 3000) {
+    super();
+  }
 
-	public init(): void {
-		this.setupExpress();
-		this.setupControllers();
-	}
+  public async init(): Promise<void> {
+    this.setupExpress();
+    this.setupControllers();
+    await this.databaseSetup();
+  }
 
-	private setupExpress(): void {
-		this.app.use(bodyParser.json());
-	}
+  private setupExpress(): void {
+    this.app.use(bodyParser.json());
+  }
 
-	private setupControllers(): void {
-		const entriesController = new EntriesController();
-		this.addControllers([entriesController]);
-	}
+  private setupControllers(): void {
+    const entriesController = new EntriesController();
+    const usersController = new UsersController();
+    this.addControllers([entriesController, usersController]);
+  }
 
-	public getApp(): Application {
-		return this.app;
-	}
+  private async databaseSetup(): Promise<void> {
+    await database.connect();
+  }
+
+  public async close(): Promise<void> {
+    await database.close();
+  }
+
+  public getApp(): Application {
+    return this.app;
+  }
 }
